c1f700f7a0d96f9f21c28a8218972feb29c35950
Naritsumi <haridian6bnaza09@gmail.com>
2023-01-10 10:43:49 +0000

    Fixed issues

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# password-generator
-- A random password generator, doesn't use random module for security reasons.
+Python password Generator
+========================
+
+ A random password generator, doesn't use random module for security reasons.
+
+This project is a tool that will allow you to generate random passwords according to the size you choose and the needs (uppercase, lowercase, symbols, numbers)
+
+---------------
+
+after cloning the project locally, move to the directory to be able to perform all operations
+
+Installation 
+============
+To install all project dependancies, currently there is no dependancy, you only need Python >=3.5
+
+```
+make init 
+```
+
+---
+Run
+===
+
+```
+make run
+```
+
+---------------
+
--- password.py
+++ password.py
@@ -5,68 +5,77 @@ import os
 yes_choices = ['YES', 'Y', 'y', 'yes']
 no_choices = ['NO', 'N', 'n', 'yes']
 
-print('\nGoing to generate a unique password...\n')
-rangeInput = 1
-rangeLength = int(input("Number of characters: "))
+def menu():
+    print('\nGoing to generate a unique password...\n')
+    rangeInput = 1
+    rangeLength = int(input("Number of characters: "))
 
-def generate_password(length: int, symbols: bool, uppercase: bool):
-    combination = string.ascii_lowercase + string.digits
+    def generate_password(length: int, symbols: bool, uppercase: bool):
+        combination = string.ascii_lowercase + string.digits
 
-    if symbols:
-        combination += string.punctuation
-    if uppercase:
-        combination += string.ascii_uppercase
+        if symbols:
+            combination += string.punctuation
+        if uppercase:
+            combination += string.ascii_uppercase
 
-    combination_length = len(combination)
+        combination_length = len(combination)
 
-    new_password = ""
+        new_password = ""
 
-    for _ in range(length):
-        new_password += combination[secrets.randbelow(combination_length)]
-    return new_password
+        for _ in range(length):
+            new_password += combination[secrets.randbelow(combination_length)]
+        return new_password
 
-#Se podría optimizar y unificar en un solo método
-def evaluateSymbolsOrNot():
-    symbols = input("Do you want to include Symbols? [Y/N] ")
-    if symbols in yes_choices:
-        return True
-    elif symbols in no_choices:
-        return False
+    #Se podría optimizar y unificar en un solo método
+    def evaluateSymbolsOrNot():
+        symbols = input("Do you want to include Symbols? [Y/N]: ")
+        if symbols in yes_choices:
+            return True
+        elif symbols in no_choices:
+            return False
+        else:
+            print('Type Y or N')
+        return symbols
+
+    symbolsOrNot = evaluateSymbolsOrNot()
+
+    def evaluateUppercaseOrNot():
+        uppercase = input("Do you want to include Uppercase letters? [Y/N]: ")
+        if uppercase in yes_choices:
+            return True
+        elif uppercase in no_choices:
+            return False
+        else:
+            print('Type Y or N')
+        return uppercase
+
+    uppercaseOrNot = evaluateUppercaseOrNot()
+
+    for _, index in enumerate(range(rangeInput)):
+        password = generate_password(length=rangeLength, symbols=symbolsOrNot, uppercase=uppercaseOrNot)
+        print(index + 1, ">>", password)    
+
+    if len(password) < 8 or password.lower() == password or password.upper() == password or password.isalnum()\
+            or not any(i.isdigit() for i in password):
+        print('\nYour password is WEAK')
+
     else:
-        print('Type Y or N')
-    return symbols
+        print('\nYour password is STRONG')
 
-symbolsOrNot = evaluateSymbolsOrNot()
+    command = 'echo ' + password.strip() + '| clip'        
+    os.system(command)
+    print('Password copied to clipboard')
 
-def evaluateUppercaseOrNot():
-    uppercase = input("Do you want to include Uppercase letters? [Y/N] ")
-    if uppercase in yes_choices:
-        return True
-    elif uppercase in no_choices:
-        return False
-    else:
-        print('Type Y or N')
-    return uppercase
+    def regeneratePassword():
+        regenerate = input("\nDo you want to regenerate password? [Y/N] ")
+        if regenerate in yes_choices:
+            return menu()     
+        elif regenerate in no_choices:        
+            print('Ok, bye :)\n')
+            exit()
+        else:
+            print('Type Y or N')
 
-uppercaseOrNot = evaluateUppercaseOrNot()
+    regeneratePassword()
 
-for _, index in enumerate(range(rangeInput)):
-    password = generate_password(length=rangeLength, symbols=symbolsOrNot, uppercase=uppercaseOrNot)
-    print(index + 1, ">>", password)
-
-command = 'echo ' + password.strip() + '| clip'        
-os.system(command)
-print('Password copied to clipboard')
-
-def copyToClipboard():
-    regenerate = input("Do you want to regenerate password? [Y/N] ")
-    if regenerate in yes_choices:
-        #return menu()             
-        print('\nOk..')
-    elif regenerate in no_choices:        
-        print('\nOk, bye :)')
-        exit()
-    else:
-        print('Type Y or N')
-
-copyToClipboard()
+menu()
