d99c3dec3ba05d232bc547d3224dc85307dd0199
Naritsumi <haridian6bnaza09@gmail.com>
2023-01-10 09:57:03 +0000

    Boolean input fixed

--- password.py
+++ password.py
@@ -1,17 +1,16 @@
 import string 
 import secrets
+import os
 
-rangeInput = int(input())
-rangeLength = int(input())
-symbolsOrNot = bool(input())
-uppercaseOrNot = bool(input())
+print('\nGoing to generate a unique password...\n')
+rangeInput = 1
+rangeLength = int(input("Number of characters: "))
 
 def generate_password(length: int, symbols: bool, uppercase: bool):
     combination = string.ascii_lowercase + string.digits
 
     if symbols:
         combination += string.punctuation
-
     if uppercase:
         combination += string.ascii_uppercase
 
@@ -21,9 +20,33 @@ def generate_password(length: int, symbols: bool, uppercase: bool):
 
     for _ in range(length):
         new_password += combination[secrets.randbelow(combination_length)]
-
     return new_password
+
+def evaluateSymbolsOrNot():
+    symbols = input("Do you want to include Symbols? [Y/N] ")
+    if symbols == "Y":
+        return True
+    if symbols == "N":
+        return False
+    return symbols
+
+symbolsOrNot = evaluateSymbolsOrNot()
+
+def evaluateUppercaseOrNot():
+    uppdercase = input("Do you want to include Uppercase letters? [Y/N] ")
+    if uppdercase == "Y":
+        return True
+    if uppdercase == "N":
+        return False
+    return uppdercase
+
+uppercaseOrNot = evaluateUppercaseOrNot()
 
 for _, index in enumerate(range(rangeInput)):
     password = generate_password(length=rangeLength, symbols=symbolsOrNot, uppercase=uppercaseOrNot)
     print(index + 1, ">>", password)
+
+command = 'echo ' + password.strip() + '| clip'
+#os.system(command)
+
+print('Password copied to clipboard')
